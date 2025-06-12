85005abfbdd8de9966b67a8b110e60665c4cbbe8
Michelle Rossouw <michelleros128@gmail.com>
2025-06-13 04:07:07 +1000

    update

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 
-nice python3 ../../Tools/autotest/sim_vehicle.py -D -G --aircraft test --vehicle ArduPlane -f quadplane -L SpringValley2 --map $@ #2> /tmp/mavproxy_cliwarnings.log
+nice python3 ../../Tools/autotest/sim_vehicle.py -D -G --aircraft test --vehicle ArduPlane -f quadplane -L SpringValley2 --map $@ 2> /tmp/mavproxy_cliwarnings.log
 
 #for ottano use SpringValley3
 #for griffin pro use SpringValley2
+
+#add --no-configure -N for no build or configure
