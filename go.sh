e12d9ff238809f452a0ab4e7942a2d2e86b62d85
Michelle Rossouw <michelleros128@gmail.com>
2025-06-12 19:01:58 +1000

    init

---
+++ go.sh
@@ -0,0 +1,5 @@
+
+nice python3 ../../Tools/autotest/sim_vehicle.py -D -G --aircraft test --vehicle ArduPlane -f quadplane -L SpringValley2 --map $@ #2> /tmp/mavproxy_cliwarnings.log
+
+#for ottano use SpringValley3
+#for griffin pro use SpringValley2
