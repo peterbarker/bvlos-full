0775485e5123fae549a68d026ce5aa184037af04
Michelle Rossouw <michelleros128@gmail.com>
2025-06-24 19:35:51 +1000

    Mt Tennent tests

--- go.sh
+++ go.sh
@@ -1,7 +1,15 @@
 
-nice python3 ../../Tools/autotest/sim_vehicle.py -D -G --aircraft test --vehicle ArduPlane -f quadplane -L SpringValley2 --map $@ 2> /tmp/mavproxy_cliwarnings.log
+if [ -z "$1" ]; then
+    LOC="SpringValley2"
+else
+    LOC="$1"
+fi
+shift
+
+nice python3 ../../Tools/autotest/sim_vehicle.py -D -G --aircraft test --vehicle ArduPlane -f quadplane -L $LOC --map $@ 2> /tmp/mavproxy_cliwarnings.log
 
 #for ottano use SpringValley3
 #for griffin pro use SpringValley2
+#Custom: MtTennent=-35.54994219,149.04469406,1369,0
 
 #add --no-configure -N for no build or configure
