598d469aec9cee1f6e5744a29d5974d6eb2edb31
Michelle Rossouw <michelleros128@gmail.com>
2025-07-02 21:18:05 +1000

    Add shortcut mission and mission we flew in on griffin pro.

--- go.sh
+++ go.sh
@@ -10,6 +10,8 @@ nice python3 ../../Tools/autotest/sim_vehicle.py -D -G --aircraft test --vehicle
 
 #for ottano use SpringValley3
 #for griffin pro use SpringValley2
-#Custom: MtTennent=-35.54994219,149.04469406,1369,0
+#Custom (put in ~/.config/ardupilot/locations.txt): 
+#MtTennent=-35.54994219,149.04469406,1369,0
+#Shortcut=-35.30414085,148.89860101,649,180
 
 #add --no-configure -N for no build or configure
